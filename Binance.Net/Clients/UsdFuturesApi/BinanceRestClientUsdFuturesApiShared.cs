4adc8cd859901df9293df5ce2351cfe4570e07b2
Jkorf <jankorf91@gmail.com>
2026-08-14 12:01:37 +0200

    Fixed Shared GetOpenInterestAsync marked as Authenticated

--- Binance.Net/Clients/UsdFuturesApi/BinanceRestClientUsdFuturesApiShared.cs
+++ Binance.Net/Clients/UsdFuturesApi/BinanceRestClientUsdFuturesApiShared.cs
@@ -979,7 +979,7 @@ namespace Binance.Net.Clients.UsdFuturesApi
 
         #region Open Interest client
 
-        GetOpenInterestOptions IOpenInterestRestClient.GetOpenInterestOptions { get; } = new GetOpenInterestOptions(_exchangeName, true);
+        GetOpenInterestOptions IOpenInterestRestClient.GetOpenInterestOptions { get; } = new GetOpenInterestOptions(_exchangeName, false);
         async Task<HttpResult<SharedOpenInterest>> IOpenInterestRestClient.GetOpenInterestAsync(GetOpenInterestRequest request, CancellationToken ct)
         {
             var validationError = SharedClient.GetOpenInterestOptions.ValidateRequest(request, this);
